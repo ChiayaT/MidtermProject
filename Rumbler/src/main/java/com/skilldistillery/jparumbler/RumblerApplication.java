e3d4c64bb009de6dbee9c01bb259e5f3ae67e089
Chiaya T <709210chiaya@gmail.com>
2023-09-22 10:34:20 -0600

    Create Boot MVC project, configure project ct dependency

---
+++ Rumbler/src/main/java/com/skilldistillery/jparumbler/RumblerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jparumbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumblerApplication.class, args);
+	}
+
+}
