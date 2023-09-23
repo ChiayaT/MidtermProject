648ec61c961d51e3d7edaa744d7db43182bffd29
TJReid9 <tjreid20@icloud.com>
2023-09-23 09:51:30 -0600

    LocationType entity created, and test passing

---
+++ JPARumbler/src/test/java/com/skilldistillery/jparumbler/entities/LocationTypeTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.jparumbler.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LocationTypeTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private LocationType locationType;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPARumbler");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		locationType = em.find(LocationType.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		locationType = null;
+	}
+
+	@Test
+	void test_LocationType_entity_basic_mapping() {
+		assertNotNull(locationType);
+		assertEquals("Business", locationType.getName());
+
+	}
+
+}
