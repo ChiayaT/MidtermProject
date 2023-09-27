1b5643c9df6518e1de28fe1df052b36624cf8f39
Cody Comstock <mr.codysherwood@hotmail.com>
2023-09-27 09:58:02 -0500

    branching issues

--- Rumbler/src/main/java/com/skilldistillery/jparumbler/controllers/RumbleController.java
+++ Rumbler/src/main/java/com/skilldistillery/jparumbler/controllers/RumbleController.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.jparumbler.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.catalina.Host;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,35 +20,45 @@ import com.skilldistillery.jparumbler.entities.User;
 
 @Controller
 public class RumbleController {
-	
+
 	@Autowired
 	private RumbleDAO rumDao;
-	
+
 	@Autowired
 	private UserDAO dao;
 
-	@RequestMapping(path = "makeRumble.do", method=RequestMethod.GET)
+	@RequestMapping(path = "makeRumble.do", method = RequestMethod.GET)
 	private String goToCreateRumble(HttpSession session) {
+		
 		session.setAttribute("disciplines", rumDao.getAllDisciplines());
 		session.setAttribute("locations", rumDao.getAllLocations());
-		session.setAttribute("locationTypes",rumDao.getAllLocationTypes());
+		session.setAttribute("locationTypes", rumDao.getAllLocationTypes());
 		session.setAttribute("guest", dao.findUserById(3));
+		// Change guest later on
 		return "CreateRumble";
 	}
-	
-	@RequestMapping(path = "makeRumble.do", method=RequestMethod.POST)
-	private String CreateRumble(HttpSession session, Rumble rumble, Integer locationId, Integer disciplineId) {
+
+	@RequestMapping(path = "makeRumble.do", method = RequestMethod.POST)
+	private String CreateRumble(HttpSession session, Rumble rumble, Integer locationId, Integer disciplineId,
+			Location location, Integer locationTypeId ) {
 		rumble.setHost((User) session.getAttribute("loggedInUser"));
 		rumble.setGuest((User) session.getAttribute("guest"));
-		if (locationId != null & disciplineId != null) {
-		rumble.setLocation(rumDao.findlocationById(locationId));
 		rumble.setDiscipline(rumDao.findDisciplineById(disciplineId));
-		Rumble newRumble = rumDao.createRumble(rumble);	
-		session.setAttribute("Rumble", newRumble);
-		session.setAttribute("loggedInUser", rumble.getHost());
-		return "Rumble";
+		if ((locationId != null & disciplineId != null) || location.getName() != null) {
+			if (locationId != null & disciplineId != null) {
+				rumble.setLocation(rumDao.findlocationById(locationId));
+			}
+			else {
+				location.setLocationType(rumDao.findLocoTypeById(locationTypeId));
+				rumDao.createLocation(location);
+				rumble.setLocation(location); 
+			}
+			Rumble newRumble = rumDao.createRumble(rumble);
+			session.setAttribute("Rumble", newRumble);
+			session.setAttribute("loggedInUser", rumble.getHost());
+			return "Rumble";
 		}
-		return"CreateRumble";
+		return "CreateRumble";
 	}
 	
 	@RequestMapping(path = "Rumble.do")
@@ -54,14 +67,14 @@ public class RumbleController {
 		session.setAttribute("Rumble", rumble);
 		return "Rumble";
 	}
-	
+
 	@RequestMapping(path = "updateRumble.do")
 	private String updateRumble(HttpSession session, Rumble rumble) {
 		Rumble updatedRumble = rumDao.updateRumble(rumble);
 		session.setAttribute("Rumble", updatedRumble);
 		return "Rumble";
 	}
-	
+
 	@RequestMapping(path = "deleteRumble.do")
 	private String deleteRumble(HttpSession session, Rumble rumble) {
 		Rumble updatedRumble = rumDao.updateRumble(rumble);
@@ -69,8 +82,21 @@ public class RumbleController {
 		return "Account";
 	}
 	
-	
-	
-	
-	
+	 @RequestMapping(path = "getLocationsList.do")
+	 private String getLocationsList(Model model) {
+		 List<Location> locations = rumDao.getAllLocations();
+		 model.addAttribute("locations", locations);
+		return "LocationsList";
+		 
+	 }
+	 @RequestMapping(path = "getLocation.do")
+	 private String getLocation(Model model, int locationId) {
+		 Location location = rumDao.findlocationById(locationId);
+		 model.addAttribute("location", location);
+		return "Location";
+	 }
+	 
+	 
+
+
 }
